2d12b1724d7b2e23ec8b695c8bceaaafbf62dc4b
Ufuk Buyuksahin <mehmetufuk.buyuksahin@nxp.com>
2019-04-12 13:39:11 +0200

    Added notification icon.

--- SerialList/MainWindow.xaml.cs
+++ SerialList/MainWindow.xaml.cs
@@ -1,16 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.IO.Ports;
+using System.Collections.Generic;
+using Hardcodet.Wpf.TaskbarNotification;
 
 namespace SerialList
 {
@@ -28,18 +20,75 @@ namespace SerialList
             timer.Start();
         }
 
+        protected override void OnStateChanged(EventArgs e)
+        {
+            if (WindowState == WindowState.Minimized)
+            {
+                Hide();
+                taskbarIcon.Visibility = Visibility.Visible;
+            }
+            else
+                taskbarIcon.Visibility = Visibility.Hidden;
+
+            base.OnStateChanged(e);
+        }
+
+        private List<string> known_ports = new List<string>();
+        private bool isThereNewPort()
+        {
+            string[] ports = SerialPort.GetPortNames();
+            if (ports.Length != known_ports.Count)
+                return true;
+          
+            foreach (string port in ports)
+            {
+                if (!known_ports.Contains(port))                
+                    return true;                
+            }
+            return false;
+        }
+
+        private void pop_up()
+        {
+            if (WindowState != WindowState.Minimized)
+                return;
+            
+            if (!isThereNewPort())
+                return;
+
+            known_ports.Clear();
+            known_ports.AddRange(SerialPort.GetPortNames());
+            string str = "";
+            foreach (string port in known_ports)
+                str += port + "\r\n";
+            if (str.Length == 0) str = "No com port";
+            taskbarIcon.ShowBalloonTip("Ports", str, BalloonIcon.Info);
+            taskbarIcon.ToolTipText = str;
+        }
+                
         private void Timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
         { 
             string str = "";
-            string[] ports = System.IO.Ports.SerialPort.GetPortNames();
+            string[] ports = SerialPort.GetPortNames();
             foreach (string port in ports)
                 str += port + "\r\n";
-            textBox.Dispatcher.Invoke(new Action(() => { textBox.Text = str; }));
+            Dispatcher.Invoke(new Action(() => { textBox.Text = str; pop_up(); }));
         }
 
         private void OnTop_Click(object sender, RoutedEventArgs e)
         {
-            this.Topmost = onTop.IsChecked.Value;
+            Topmost = onTop.IsChecked.Value;
+        }
+
+        private void Minimize_btn_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void TaskbarIcon_TrayMouseDoubleClick(object sender, RoutedEventArgs e)
+        {
+            Show();
+            WindowState = WindowState.Normal;
         }
     }
 }
